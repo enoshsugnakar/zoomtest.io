3661f7d4ea86fde802717934597aff8191112bb3
enoshsugnakar <enoshsugnakar.ch@gmail.com>
2025-05-22 14:36:00 +0530

    First Commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  };
-  
+  plugins: {
+    // This tells PostCSS to load the `@tailwindcss/postcss` package
+    // under the `tailwindcss` key, with an empty options object:
+    tailwindcss: {},
+    // And load autoprefixer as usual:
+    autoprefixer: {},
+  },
+};

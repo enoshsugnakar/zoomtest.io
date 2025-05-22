c4bc32dcf4aa27dd6a0bdfa93b79bd934730e76b
enoshsugnakar <enoshsugnakar.ch@gmail.com>
2025-05-22 18:42:01 +0530

    final 1

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,14 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-console.log('🚀 Supabase URL:', supabaseUrl);
-console.log('🔑 Supabase ANON key (first 10 chars):', supabaseAnonKey.slice(0, 10));
+//console.log('🚀 Supabase URL:', supabaseUrl);
+//console.log('🔑 Supabase ANON key (first 10 chars):', supabaseAnonKey.slice(0, 10));
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 ;(async () => {
   const { data, error } = await supabase.storage.listBuckets();
-  console.log('📦 Buckets:', data, 'Error:', error);
+ // console.log('📦 Buckets:', data, 'Error:', error);
 })();
 
 export { supabase };
